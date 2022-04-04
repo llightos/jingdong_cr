295ecbc807d16460fd198449ac7677fe3963c1d5
LOS1 <1275960183@qq.com>
2022-04-04 17:10:57 +0800

    更新文档

--- main.go
+++ main.go
@@ -12,7 +12,6 @@ import (
 	"strings"
 )
 
-//const JingdongUrl = "https://dd-search.jd.com/?terminal=pc&newjson=1&ver=2&zip=1&key=爱你&pvid=27efa25060f849e38fa78170170f82ba&t=1649056874120&curr_url=search.jd.com/Search&callback=jQuery5868174"
 const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 func RandomString() string {
@@ -20,7 +19,6 @@ func RandomString() string {
 	for i := range b {
 		b[i] = letterBytes[rand.Intn(len(letterBytes))]
 	}
-
 	return string(b)
 }
 
@@ -30,7 +28,6 @@ func main() {
 	flag.Parse()
 	pvid := handle.PVid()
 	client := http.Client{}
-	//headerR := make(map[string][]string)
 	if *proxy != "null" {
 		fmt.Println("设置代理为:", *proxy)
 		transport := &http.Transport{Proxy: func(req *http.Request) (*url.URL, error) {
